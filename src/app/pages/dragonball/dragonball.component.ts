cf2e8f845501824c075ad1c87646637d5fa2577f
Roma Bilibov <romabilibov@gmail.com>
2025-04-24 01:59:18 +0200

    Create Character

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -12,6 +12,9 @@ interface Character {
   templateUrl: './dragonball.component.html'
 })
 export class DragonballComponent {
+  name = signal('Gohan')
+  power = signal(0)
+
   characters = signal<Character[]>([
     {
       id:1,
@@ -30,5 +33,25 @@ export class DragonballComponent {
     },
   ]);
 
+  addCharacter()
+  {
+    if(!this.name() || !this.power() || this.power() <= 0){
+      return
+    }
 
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+
+    this.characters.update(list => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields()
+  {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
