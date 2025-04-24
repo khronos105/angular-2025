6d36c30c15b03ee25b220e360453096774174e71
Roma Bilibov <romabilibov@gmail.com>
2025-04-24 02:59:34 +0200

    Angular Services

--- src/app/pages/dragonball-super/dragonball-super.component.ts
+++ src/app/pages/dragonball-super/dragonball-super.component.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
-import { Character } from '../../interfaces/character.interface';
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { DragonballService } from '../../services/dragonball.service';
 
 @Component({
   selector: 'app-dragonball-super',
@@ -9,23 +9,5 @@ import { CharacterAddComponent } from '../../components/dragonball/character-add
   templateUrl: './dragonball-super.component.html'
 })
 export class DragonballSuperPageComponent {
-
-  characters = signal<Character[]>([
-    {
-      id:1,
-      name: 'Goku',
-      power: 1292
-    },
-    {
-      id:2,
-      name: 'Vegeta',
-      power: 3211
-    },
-  ]);
-
-
-  addCharacter(character: Character)
-  {
-    this.characters.update(list => [...list, character])
-  }
+  dragonBallService = inject(DragonballService)
 }
