fe268a846026909de8cec2fdb66d2be2dc3d94ae
Roma Bilibov <romabilibov@gmail.com>
2025-04-24 03:05:34 +0200

    Angular Effects

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core'
+import { effect, Injectable, signal } from '@angular/core'
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -16,6 +16,10 @@ export class DragonballService
       power: 3211
     },
   ]);
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
+  })
 
   addCharacter(character: Character)
   {
